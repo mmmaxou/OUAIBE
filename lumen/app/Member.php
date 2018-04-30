54986d998bc30c81113f289e282643dd40534214
mmmaxou <mmmaxou@yahoo.fr>
2018-04-30 18:04:39 +0200

    update passport

--- lumen/app/Member.php
+++ lumen/app/Member.php
@@ -2,8 +2,9 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
+use Laravel\Passport\HasApiTokens;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
