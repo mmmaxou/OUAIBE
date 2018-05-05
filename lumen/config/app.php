10c66d5de8c93ef95d7064c84873f402935b3363
mmmaxou <mmmaxou@yahoo.fr>
2018-05-05 17:45:01 +0200

    quick commit to checkout

---
+++ lumen/config/app.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Model namespace
+    |--------------------------------------------------------------------------
+    |
+    | Specify the full namespace to your User model.
+    | e.g. 'Acme\Entities\User'
+    |
+    */
+
+    'user' => 'App\Member',
+];
