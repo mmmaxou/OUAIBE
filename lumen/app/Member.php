10ad870ec07d56d84f7f16af2b8a55466be38c1e
mmmaxou <mmmaxou@yahoo.fr>
2018-05-05 20:30:24 +0200

    ca marche putain

--- lumen/app/Member.php
+++ lumen/app/Member.php
@@ -2,60 +2,63 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Support\Facades\Hash;
+
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
 
-class Member extends Model implements AuthenticatableContract, AuthorizableContract {
+class Member extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject {
 
-  use Authenticatable,
-      Authorizable;
+    use Authenticatable,
+    Authorizable;
 
-  /**
+    /**
    * The attributes that are mass assignable.
    *
    * @var array
    */
-  protected $fillable = [
-      'id',
-      'email',
-      'firstName',
-      'lastName',
-      'phoneNumber',
-      'lastPaymentDate',
-      'role_id'
-  ];
+    protected $fillable = [
+        'id',
+        'email',
+        'firstName',
+        'lastName',
+        'phoneNumber',
+        'lastPaymentDate',
+        'role_id'
+    ];
 
-  /**
+    /**
    * The attributes excluded from the model's JSON form.
    *
    * @var array
    */
-  protected $hidden = [
-      'password',
-      'created_at',
-      'updated_at'
-  ];
+    protected $hidden = [
+        'password',
+        'created_at',
+        'updated_at'
+    ];
 
-  /**
+    /**
    * Define a one-to-many relationship with App\Role 
    */
-  public function role() {
-    return $this->belongsTo('App\Role');
-  }
+    public function role() {
+        return $this->belongsTo('App\Role');
+    }
 
-  /**
+    /**
    * Define a many-to-many relationship with App\Image 
    */
-  public function images() {
-    return $this->belongsToMany('App\Image');
-  }
+    public function images() {
+        return $this->belongsToMany('App\Image');
+    }
 
-  /**
+    /**
    * Checks if the members has a role by its name.
    *
    * @param string|array $name       Role name or array of role names.
@@ -63,32 +66,32 @@ class Member extends Model implements AuthenticatableContract, AuthorizableContr
    *
    * @return bool
    */
-  public function hasRole($name, $requireAll = false) {
-    if (is_array($name)) {
-      foreach ($name as $roleName) {
-        $hasRole = $this->hasRole($roleName);
+    public function hasRole($name, $requireAll = false) {
+        if (is_array($name)) {
+            foreach ($name as $roleName) {
+                $hasRole = $this->hasRole($roleName);
 
-        if ($hasRole && !$requireAll) {
-          return true;
-        } elseif (!$hasRole && $requireAll) {
-          return false;
+                if ($hasRole && !$requireAll) {
+                    return true;
+                } elseif (!$hasRole && $requireAll) {
+                    return false;
+                }
+            }
+
+            // If we've made it this far and $requireAll is FALSE, then NONE of the roles were found
+            // If we've made it this far and $requireAll is TRUE, then ALL of the roles were found.
+            // Return the value of $requireAll;
+            return $requireAll;
+        } else {
+            if ($this->role() == $name) {
+                return true;
+            }
         }
-      }
 
-      // If we've made it this far and $requireAll is FALSE, then NONE of the roles were found
-      // If we've made it this far and $requireAll is TRUE, then ALL of the roles were found.
-      // Return the value of $requireAll;
-      return $requireAll;
-    } else {
-      if ($this->role() == $name) {
-        return true;
-      }
+        return false;
     }
 
-    return false;
-  }
-
-  /**
+    /**
    * Check if user has a permission by its name.
    *
    * @param string|array $permission Permission string or array of permissions.
@@ -96,35 +99,35 @@ class Member extends Model implements AuthenticatableContract, AuthorizableContr
    *
    * @return bool
    */
-  public function can($permission, $requireAll = false) {
-    if (is_array($permission)) {
-      foreach ($permission as $permName) {
-        $hasPerm = $this->can($permName);
+    public function can($permission, $requireAll = false) {
+        if (is_array($permission)) {
+            foreach ($permission as $permName) {
+                $hasPerm = $this->can($permName);
 
-        if ($hasPerm && !$requireAll) {
-          return true;
-        } elseif (!$hasPerm && $requireAll) {
-          return false;
-        }
-      }
+                if ($hasPerm && !$requireAll) {
+                    return true;
+                } elseif (!$hasPerm && $requireAll) {
+                    return false;
+                }
+            }
 
-      // If we've made it this far and $requireAll is FALSE, then NONE of the perms were found
-      // If we've made it this far and $requireAll is TRUE, then ALL of the perms were found.
-      // Return the value of $requireAll;
-      return $requireAll;
-    } else {
-      // Validate against the Permission table
-      foreach ($this->role()->cachedPermissions() as $perm) {
-        if (str_is($permission, $perm->name)) {
-          return true;
+            // If we've made it this far and $requireAll is FALSE, then NONE of the perms were found
+            // If we've made it this far and $requireAll is TRUE, then ALL of the perms were found.
+            // Return the value of $requireAll;
+            return $requireAll;
+        } else {
+            // Validate against the Permission table
+            foreach ($this->role()->cachedPermissions() as $perm) {
+                if (str_is($permission, $perm->name)) {
+                    return true;
+                }
+            }
         }
-      }
+
+        return false;
     }
 
-    return false;
-  }
-
-  /**
+    /**
    * Checks role(s) and permission(s).
    *
    * @param string|array $roles       Array of roles or comma separated string
@@ -135,61 +138,81 @@ class Member extends Model implements AuthenticatableContract, AuthorizableContr
    *
    * @return array|bool
    */
-  public function ability($roles, $permissions, $options = []) {
-    // Convert string to array if that's what is passed in.
-    if (!is_array($roles)) {
-      $roles = explode(',', $roles);
-    }
-    if (!is_array($permissions)) {
-      $permissions = explode(',', $permissions);
-    }
+    public function ability($roles, $permissions, $options = []) {
+        // Convert string to array if that's what is passed in.
+        if (!is_array($roles)) {
+            $roles = explode(',', $roles);
+        }
+        if (!is_array($permissions)) {
+            $permissions = explode(',', $permissions);
+        }
 
-    // Set up default values and validate options.
-    if (!isset($options['validate_all'])) {
-      $options['validate_all'] = false;
-    } else {
-      if ($options['validate_all'] !== true && $options['validate_all'] !== false) {
-        throw new InvalidArgumentException();
-      }
-    }
-    if (!isset($options['return_type'])) {
-      $options['return_type'] = 'boolean';
-    } else {
-      if ($options['return_type'] != 'boolean' &&
-              $options['return_type'] != 'array' &&
-              $options['return_type'] != 'both') {
-        throw new InvalidArgumentException();
-      }
-    }
+        // Set up default values and validate options.
+        if (!isset($options['validate_all'])) {
+            $options['validate_all'] = false;
+        } else {
+            if ($options['validate_all'] !== true && $options['validate_all'] !== false) {
+                throw new InvalidArgumentException();
+            }
+        }
+        if (!isset($options['return_type'])) {
+            $options['return_type'] = 'boolean';
+        } else {
+            if ($options['return_type'] != 'boolean' &&
+                $options['return_type'] != 'array' &&
+                $options['return_type'] != 'both') {
+                throw new InvalidArgumentException();
+            }
+        }
 
-    // Loop through roles and permissions and check each.
-    $checkedRoles = [];
-    $checkedPermissions = [];
-    foreach ($roles as $role) {
-      $checkedRoles[$role] = $this->hasRole($role);
-    }
-    foreach ($permissions as $permission) {
-      $checkedPermissions[$permission] = $this->can($permission);
-    }
+        // Loop through roles and permissions and check each.
+        $checkedRoles = [];
+        $checkedPermissions = [];
+        foreach ($roles as $role) {
+            $checkedRoles[$role] = $this->hasRole($role);
+        }
+        foreach ($permissions as $permission) {
+            $checkedPermissions[$permission] = $this->can($permission);
+        }
 
-    // If validate all and there is a false in either
-    // Check that if validate all, then there should not be any false.
-    // Check that if not validate all, there must be at least one true.
-    if (($options['validate_all'] && !(in_array(false, $checkedRoles) || in_array(false, $checkedPermissions))) ||
+        // If validate all and there is a false in either
+        // Check that if validate all, then there should not be any false.
+        // Check that if not validate all, there must be at least one true.
+        if (($options['validate_all'] && !(in_array(false, $checkedRoles) || in_array(false, $checkedPermissions))) ||
             (!$options['validate_all'] && (in_array(true, $checkedRoles) || in_array(true, $checkedPermissions)))) {
-      $validateAll = true;
-    } else {
-      $validateAll = false;
+            $validateAll = true;
+        } else {
+            $validateAll = false;
+        }
+
+        // Return based on option
+        if ($options['return_type'] == 'boolean') {
+            return $validateAll;
+        } elseif ($options['return_type'] == 'array') {
+            return ['roles' => $checkedRoles, 'permissions' => $checkedPermissions];
+        } else {
+            return [$validateAll, ['roles' => $checkedRoles, 'permissions' => $checkedPermissions]];
+        }
     }
 
-    // Return based on option
-    if ($options['return_type'] == 'boolean') {
-      return $validateAll;
-    } elseif ($options['return_type'] == 'array') {
-      return ['roles' => $checkedRoles, 'permissions' => $checkedPermissions];
-    } else {
-      return [$validateAll, ['roles' => $checkedRoles, 'permissions' => $checkedPermissions]];
-    }
-  }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
