c27b6a2cf2418d6894c56f39646044724cc26c84
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-05-14 10:36:16 +0200

    cookies get

--- api-call/main.js
+++ api-call/main.js
@@ -1,27 +1,52 @@
+'use strict';
 
-const WEBSITE_ADDRESS = 'https://bdi.nicolassenecal.com/lumen/public/'
+const WEBSITE_ADDRESS = 'https://bdi.nicolassenecal.com/api/'
+var token
+
+const ConnexionInterface = {
+	Login: function (email, password) {		
+		return new Promise(function (resolve, reject) {
+
+			if ( !email ) {
+				reject("The email option is required\nUse : ConnextionInterface.Login(<email>, <password>);")
+			}			
+			if ( !password ) {
+				reject("The password option is required\nUse : ConnextionInterface.Login(<email>, <password>);")
+			}
+
+			let formData = new FormData()
+			formData.append("email", email)
+			formData.append("password", password)
+			const uri = WEBSITE_ADDRESS + 'login'
+			const opt = {
+				mode: 'cors',
+				method: 'POST',
+				body: formData
+			}
+			console.log(opt)
+			fetch(uri, opt)
+				.then( response => response.json() )
+				.then( json => resolve(json) )
+
+		})
+	}
+}
+
 const ApiInterface = function (routeName) {
 	var self = {
 		All: function () {
 			return new Promise( function (resolve, reject) {
 				const uri = WEBSITE_ADDRESS + routeName
-				console.log(uri)
-				let options = {
-					url:uri,
-					method:'GET',
-					mode: 'cors',
-					headers:{
-						'Access-Control-Allow-Origin':'*'
-					},
-					body:null,
-				};
-				fetch(options)
-					.then(response => response.json())
-					.then(json => resolve(json))
+				fetch(uri, {mode: 'cors'})
+					.then((response) => { 
+					return response.json()
+				})
+					.then((json) => {
+					resolve(json)
+				} )
 			})
 		}
 	}
-
 	return self
 }
 const MembresInterface = new ApiInterface('members')
@@ -31,4 +56,21 @@ $('#base').click(function () {
 		.All()
 		.then( json => console.log(json))
 })
+
+ConnexionInterface.Login("admin", "admin")
+	.then( json => {
+	console.log(json)
+	setCookie("token", json.access_token, json.expires_in)
+	token = json.access_token
+})
+	.catch( err => {
+	console.error(err)
+})
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
