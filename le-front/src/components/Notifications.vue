471b2138312464f96b0e3db49d1ae265090fb636
antoinemi <antoinemiaud@gmail.com>
2018-05-17 18:42:27 +0200

    fix topbar at the stop and begin styling

---
+++ le-front/src/components/Notifications.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.notifications
+  img.notifications__img(src="../assets/bell.png")
+  .count.notifications__count 2
+</template>
+
+<script>
+export default {
+  name: 'UserPicture',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+.notifications
+  &__count
+    position absolute
+    background #b91a43
+    width 24px
+    height 24px
+    border-radius 12px
+</style>
--- le-front/src/components/TopBar.vue
+++ le-front/src/components/TopBar.vue
@@ -1,15 +1,37 @@
 <template lang="pug">
 .topbar
-  p TOP BAR
+  img.topbar__logo(src="../assets/logo.png")
+  h1.topbar__title BDI
+  topbar__right
+    Notifications
+    User
 </template>
+
 <script>
+import Notifications from '@/components/Notifications'
+import User from '@/components/User'
 
 export default {
-  name: 'TopBar'
+  name: 'TopBar',
+  components: {
+    Notifications,
+    User
+  }
 }
-
 </script>
 
-<style>
+<style lang="stylus">
+.topbar
+  height: 100px
+  width 100%
+  background #23262d
+  display flex
+  align-items center
+  justify-content space-between
+
+  &__right
+    display flex
+    align-items center
+    justify-content space-between
 
 </style>
