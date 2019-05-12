09a736f3c01f61dcaa94cc60c79e3698ca2dac15
mmmaxou <mmmaxou@yahoo.fr>
2019-05-12 20:35:11 +0200

    All bug solved: Do not use getmdl component js and recode it

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -23,13 +23,10 @@ export default ({name, pageName, description}) => {
       <meta name='msapplication-TileImage' content='./src/template/images/touch/ms-touch-icon-144x144-precomposed.png' />
       <meta name='msapplication-TileColor' content='#3372DF' />
 
-      <link rel='shortcut icon' href='./src/template/images/favicon.png' />
-
       <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
       <link rel='stylesheet' href='https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css' />
       <link rel='stylesheet' href='./src/template/styles.css' />
-
     </head>
   )
 }
