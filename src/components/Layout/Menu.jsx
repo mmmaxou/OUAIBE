9be38138e1c403104e1a3fb8e7b9313c92aaabc0
mmmaxou <mmmaxou@yahoo.fr>
2019-05-13 02:27:09 +0200

    adding a dropdown Componenet

--- src/components/Layout/Menu.jsx
+++ src/components/Layout/Menu.jsx
@@ -1,5 +1,9 @@
 import { h } from 'hyperapp'
 import { Link } from '@hyperapp/router'
+import Dropdown from '../Generic/Dropdown'
+
+/** EXEMPLE */
+const fakeGenericDisconnectHandler = () => console.log('TODO !!!')
 
 export default () => (
   <div class='bdi-drawer mdl-layout__drawer'>
@@ -9,16 +13,11 @@ export default () => (
         <div class='mdl-layout-spacer'></div>
         <img src='/src/images/ios-desktop.png' class='bdi-avatar' />
       </div>
-      <div class='bdi-avatar-dropdown'>
-        <span><strong>Guillaume Lollier</strong></span>
-        <div class='mdl-layout-spacer'></div>
-        <button id='accbtn' class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon'>
-          <i class='material-icons' role='presentation'>arrow_drop_down</i>
-        </button>
-        <ul class='mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect' for='accbtn'>
-          <li class='mdl-menu__item'>Déconnexion</li>
-        </ul>
-      </div>
+      <Dropdown
+        message='Guillaume Lollier'
+        options={[
+          { message: 'Déconnexion', handler: fakeGenericDisconnectHandler }
+        ]} />
     </header>
     <nav class='bdi-navigation mdl-navigation'>
       <Link to='/' class='mdl-navigation__link'>
